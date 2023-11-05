5df85cd3d20656643067036cd93460829dc60f27
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2023-11-05 11:04:14 -0500

    script added variables and comments to outline rough plan

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//The base URL should look like the following:
+//     https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}.
+//API call using city name, state code, country code-->
+//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}-->
+//API key c7454d856275732eafa834406c0b2f1a-->
+$(function () {
+    const apiKey = "c7454d856275732eafa834406c0b2f1a";
+})
